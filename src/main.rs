a806c54c64820a21421d38836d3bb428ddc6eb96
Simon Hitzemann <simon@hitzemann.org>
2015-12-18 09:56:26 +0100

    Added outputs for problems 2-4

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ mod euler;
 
 fn main() {
 	println!("Problem 1: {}", euler::problem0001(1000));
+	println!("Problem 2: {}", euler::problem0002(4000000));
+	println!("Problem 3: {}", euler::problem0003(600851475143));
+	println!("Problem 4: {}", euler::problem0004(3));
 }
